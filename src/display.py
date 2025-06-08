27358feb05c0bee0a1984cd0b373856e968baeb4
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-06-08 21:58:41 +0500

    refactor(display): add carpark attribute and reorder for default values

--- src/display.py
+++ src/display.py
@@ -1,12 +1,13 @@
 class Display:
-    def __init__(self, weather, message="", id, is_on=False):
+    def __init__(self, weather, id, car_park, message="", is_on=False):
         self.weather = weather
-        self.message = message
         self.id = id
+        self.car_park = car_park
         self.is_on = is_on
+        self.message = message
 
     def __str__(self):
-        return f"Display {self.id}: Welcome to the carpark."
+        return f"Display {self.id}: Welcome to the {self.car_park.location} carpark."
 
     def display_bays(self, available_bays):
         # Minimal idea presented here.
