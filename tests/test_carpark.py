fd79c50926c984ca4df47025f786b0f8033e0436
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-06-12 14:45:21 +0500

    test: add carpark tests

---
+++ tests/test_carpark.py
@@ -0,0 +1,45 @@
+import unittest
+from carpark import CarPark
+from car import Car
+from sensor import Sensor
+from display import Display
+
+class TestCarPark(unittest.TestCase):
+    def setUp(self):
+        self.carpark = CarPark("Belmont", 25)
+        self.car = Car()
+        self.sensor = Sensor(1, self.carpark)
+        self.display = Display(2, self.carpark)
+
+
+        
+
+    def test_car_park_initialised_with_all_attributes(self):
+        self.assertIsInstance(self.carpark, CarPark)
+        self.assertEqual(self.carpark.location, "Belmont")
+        self.assertEqual(self.carpark.capacity, 25)
+        self.assertEqual(self.carpark.filled_bays, 0)
+        self.assertEqual(self.carpark.available_bays, 25)
+        self.assertEqual(self.carpark.license_plates, [])
+        self.assertEqual(self.carpark.displays, [])
+        self.assertEqual(self.carpark.sensor, [])
+
+    def test_license_plate_is_added_and_removed_from_database(self):
+        self.carpark.update_plate_database(self.car)
+        self.assertIn(self.car._license_plate, self.carpark.license_plates)
+        self.carpark.update_plate_database(self.car)
+        self.assertNotIn(self.car._license_plate, self.carpark.license_plates)
+
+    def test_register_sensor(self):
+        self.carpark.register_component(self.sensor)
+        self.assertIn(self.sensor, self.carpark.sensor)
+
+    def test_register_display(self):
+        self.carpark.register_component(self.display)
+        self.assertIn(self.display, self.carpark.displays)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
