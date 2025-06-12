fba02be5b59583c8047935e8be99e07b18024611
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-06-12 21:49:14 +0500

    tests(display): add wrong argument test

--- tests/test_display.py
+++ tests/test_display.py
@@ -16,7 +16,7 @@ class TestDisplay(unittest.TestCase):
     def test_display_formatting_correctly(self):
         self.assertEqual(self.display.display_data("available_bays", 30), "Available bays: 30")
 
-    def test_error_when_wrong_argument_in_carpark_update_display(self):
+    def test_error_when_wrong_argument_provided_to_display(self):
         with self.assertRaises(ValueError):
             self.carpark.update_display("time", 1255)
          
