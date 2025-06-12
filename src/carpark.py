5ed6ba766f0b593ce628e666d5a55b0aed00ea04
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-06-12 14:37:23 +0500

    refactor: filled_bays equal to length of cars list

--- src/carpark.py
+++ src/carpark.py
@@ -2,24 +2,24 @@ from sensor import Sensor
 from display import Display
 
 class CarPark:
-    def __init__(self, location, capacity, filled_bays=0, license_plates=None, displays=None, sensor=None, cars=None):
+    def __init__(self, location, capacity, license_plates=None, displays=None, sensor=None, cars=None):
         self.location = location
         self.capacity = capacity
-        self.filled_bays = filled_bays
-        self.available_bays = capacity - filled_bays
-        self.license_plates = license_plates
-        self.displays = displays
-        self.sensor = sensor
-        self.cars = cars
+        self.filled_bays = len(self.cars)
+        self.available_bays = capacity - self.filled_bays
+        self.license_plates = license_plates or []
+        self.displays = displays or []
+        self.sensor = sensor or []
+        self.cars = cars or []
 
     def __str__(self):
         return f"{self.location} carpark has a max capacity of {self.capacity}"
 
     def update_plate_database(self, car):
-        if car.license_plate in self.license_plates:
-            self.license_plates.remove(car.license_plate)
+        if car._license_plate in self.license_plates:
+            self.license_plates.remove(car._license_plate)
         else:
-            self.license_plates.append(car.license_plate)
+            self.license_plates.append(car._license_plate)
         self.update_displays()
 
 
